de7c58cbcf2ff3f8736cd17b9a429a7d3362e767
ecabott <ayush.bhusal0@gmail.com>
2023-07-10 23:02:02 +0545

    todo/subtask db operations

---
+++ backend/modules/subtasks/subtask.routes.api.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const Controller = require("./subtask.controller");
+
+// create subtask
+router.post("/", async (q, r, n) => {
+  const payload = q.body || {};
+  Controller.add(payload)
+    .then((d) => r.json(d))
+    .catch((e) => n(e));
+});
+
+// list subtask by id
+router.get("/:id", async (q, r, n) => {
+  Controller.getById(q.params.id)
+    .then((d) => r.json(d))
+    .catch((e) => n(e));
+});
+
+// update subtask status
+router.put("/:id", async (q, r, n) => {
+  const payload = q.body;
+  Controller.update(q.params.id, payload)
+    .then((d) => r.json(d))
+    .catch((e) => n(e));
+});
+
+// delete subtask
+router.delete("/:id", async (q, r, n) => {
+  Controller.remove(q.params.id)
+    .then((d) => r.json(d))
+    .catch((e) => n(e));
+});
+
+module.exports = router;
---
+++ backend/modules/todos/todo.controller.js
@@ -0,0 +1,48 @@
+const Model = require("./todo.model");
+const SubtaskModel = require("../subtasks/subtask.model");
+const { update } = require("../subtasks/subtask.controller");
+
+class Controller {
+  add(payload) {
+    if (!payload) throw new Error("Must send some Payload");
+    return Model.create(payload);
+  }
+
+  async list() {
+    try {
+      const query = [];
+      query.push({
+        $lookup: {
+          from: "subtasks",
+          localField: "_id",
+          foreignField: "todo_id",
+          as: "subtasks",
+        },
+      });
+      return Model.aggregate(query);
+    } catch (err) {
+      return { message: err.message };
+    }
+  }
+
+  getById(id) {
+    return Model.findById(id);
+  }
+
+  async update(id, status) {
+    const allSubtasks = await SubtaskModel.find({
+      todo_id: id,
+      status: "pending",
+    });
+    allSubtasks.map(async (d) => {
+      const resp = await update(d.id, status);
+      return resp;
+    });
+    return Model.findByIdAndUpdate(id, status, { new: true });
+  }
+
+  remove(id) {
+    return Model.findByIdAndRemove(id);
+  }
+}
+module.exports = new Controller();
