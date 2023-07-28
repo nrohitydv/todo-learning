0c6ef66e928ce29e517e8e8e3ca710f525dabdee
Rohit <rohityadav787898@gmail.com>
2023-07-28 13:38:21 +0545

    cloned todoapp from bushal

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const routeManager = require("./routes");
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
